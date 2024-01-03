07def17941f5237114b61b6d439a2fbc57370f3a
Adrienne Rio <riowongsoatmojo@gmail.com>
2024-01-03 02:34:17 +0000

    Update Editor.tsx

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -45,6 +45,64 @@ const ENTRY_FILE = `
 </html>
 `;
 
+const GENERATED_APP_FILE = `
+export default function() {
+    let n = 0;
+
+    function onClick() {
+        _invalidate('n', n += 1);
+    }
+    let App_0;
+    let h1_0;
+    let text_0;
+    let expr_0;
+    let button_0;
+    let text_1;
+    let variableToNodesMapper = new Map();
+
+    function _invalidate(variable, expr) {
+        const nodes = variableToNodesMapper.get(variable);
+        if (nodes) {
+            for (let i = 0; i < nodes.length; i++) {
+                nodes[i]();
+            }
+        }
+    }
+
+    function _listen(variable, fn) {
+        if (!variableToNodesMapper.has(variable))
+            variableToNodesMapper.set(variable, []);
+        variableToNodesMapper.set(variable, [
+            ...variableToNodesMapper.get(variable),
+            fn,
+        ]);
+    }
+    _listen('n', () => expr_0.data = n);
+    return {
+        create() {
+            App_0 = document.createElement('div');
+            document.body.appendChild(App_0)
+            App_0.setAttribute('data-component', 'App')
+            h1_0 = document.createElement('h1');
+            App_0.appendChild(h1_0)
+            text_0 = document.createTextNode('The number is ;')
+            h1_0.appendChild(text_0);
+            expr_0 = document.createTextNode(n)
+            h1_0.appendChild(expr_0)
+            button_0 = document.createElement('button');
+            App_0.appendChild(button_0)
+            text_1 = document.createTextNode('Increment;')
+            button_0.appendChild(text_1);
+            button_0.addEventListener('click', onClick);
+        },
+        destroy() {
+            const component = document.querySelector('[data-component="Counter_0"]');
+            document.body.removeChild(component);
+        }
+    }
+}
+`
+
 async function compileApp(code: string) {
     const generatedCode = await Sunsettia.compileRaw(code);
     return generatedCode;
@@ -96,7 +154,7 @@ const Editor = () => {
                     code: APP_FILE,
                 },
                 "/app.js": {
-                    code: "",
+                    code: GENERATED_APP_FILE,
                 },
             }}
         >
