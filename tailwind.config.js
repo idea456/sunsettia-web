7858ba2847785368e388404df0be2546083eaa4e
Adrienne Rio <riowongsoatmojo@gmail.com>
2024-01-03 03:34:42 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {
-            fontFamily: {
-                header: ["Alpino"],
-                "header-two": ["Zodiak"],
-            },
-            keyframes: {
-                roll: {
-                    "0%": {
-                        transform: "translateX(-1px)",
-                    },
-                    "100%": {
-                        transform: "translateX(-60px)",
-                    },
-                },
-            },
-            animation: {
-                rolling: "roll 3s linear infinite",
-            },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        header: ["Alpino"],
+        "header-two": ["Zodiak"],
+      },
+      keyframes: {
+        roll: {
+          "0%": {
+            transform: "translateX(-1px)",
+          },
+          "100%": {
+            transform: "translateX(-60px)",
+          },
         },
+      },
+      animation: {
+        rolling: "roll 3s linear infinite",
+      },
+      screens: {
+        "3xl": "2300px",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
